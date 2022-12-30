31ded14e88bb04522433c9d67fe016e7188e4f64
Abdallah Askar <abdoasker.1426@gmail.com>
2022-12-30 21:28:38 +0200

    update monthes in ChartBar

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -3,14 +3,18 @@ import Chart from '../Chart/Chart';
 
 const ExpenseChart =props => {
     const chartDataPoints=[
-        {label:"jan",value:0},
-        {label:"feb",value:0},
-        {label:"mar",value:0},
-        {label:"apr",value:0},
-        {label:"jan",value:0},
-        {label:"jan",value:0},
-        {label:"jan",value:0},
-        {label:"jan",value:0}
+        {label:"Jan",value:0},
+        {label:"Feb",value:0},
+        {label:"Mar",value:0},
+        {label:"Apr",value:0},
+        {label:"May",value:0},
+        {label:"June",value:0},
+        {label:"July",value:0},
+        {label:"Aug",value:0},
+        {label:"Sept",value:0},
+        {label:"Oct",value:0},
+        {label:"Nov",value:0},
+        {label:"Dec",value:0},
     ]
 
     for (const expense of props.expenses){
